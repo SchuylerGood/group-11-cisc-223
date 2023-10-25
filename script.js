ba94fd8232728d01f1aefbeff0deae9062e4f20d
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2023-10-25 16:20:00 -0400

    added assignment 1

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeA1() {
+    document.getElementById("assignemnt").src="a1.html";
+}
+
+function changeA2() {
+    document.getElementById("assignemnt").src="a2.html";
+}
+
+function changeA3() {
+    document.getElementById("assignemnt").src="a3.html";
+}
+
+function changeA4() {
+    document.getElementById("assignemnt").src="a4.html";
+}
